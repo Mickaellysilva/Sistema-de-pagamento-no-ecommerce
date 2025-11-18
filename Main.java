849a42f00345936d5fbc8b2800842107da1fc463
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2025-11-18 10:10:34 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
-public abstract class Pagamento {
-    public void processarPagamento(double valor){};
+public class Main {
+    public static void main(String[] args) {
+        Pix p1 = new Pix("mickaellysrs@gmail.com", "Nubank");
+        p1.processarPagamento(250);
+    }
 }
